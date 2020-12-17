74ac1b54a0000353f83959b08e39dc6c31e7b53c
masayan1126 <masa199311266@gmail.com>
2020-12-17 12:55:41 +0900

    クライアントサイドナビゲーションとcardページの実装

---
+++ pages/components/cards.jsx
@@ -0,0 +1,12 @@
+import React, { useCallback, useState, useEffect } from "react";
+import Card from "../../components/Card";
+
+const Cards = () => {
+  return (
+    <section>
+      <Card />
+    </section>
+  );
+};
+
+export default Cards;
