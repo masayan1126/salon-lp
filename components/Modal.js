eba22eac1dfe50f3f6ecc9dc4020af0572a7c417
masayan1126 <masa199311266@gmail.com>
2020-12-18 20:45:27 +0900

    テスト

--- components/Modal.js
+++ components/Modal.js
@@ -28,9 +28,9 @@ const Modal = (props) => {
                   </h3>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Are you sure you want to deactivate your account? All of
-                      your data will be permanently removed. This action cannot
-                      be undone.
+                      spanAre you sure you want to deactivate your account? All
+                      of your data will be permanently removed. This action
+                      cannot be undone.
                     </p>
                   </div>
                 </div>
@@ -41,6 +41,7 @@ const Modal = (props) => {
                 type="button"
                 className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                 onClick={() => props.setIsDisplay(false)}
+                className={modalDisplay}
               >
                 Deactivate
               </button>
