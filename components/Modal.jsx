5806d1d01c3ab8168073d4dc17d8f342777725b3
masayan1126 <masa199311266@gmail.com>
2020-12-18 19:06:26 +0900

    test

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -10,12 +10,6 @@ const Modal = (props) => {
             <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
           </div>
 
-          <span
-            class="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <div
             class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
             role="dialog"
