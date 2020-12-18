5506760e2d546c6e3a2a4c721073bc597110adcd
masayan1126 <masa199311266@gmail.com>
2020-12-18 18:53:53 +0900

    カレンダー画面とモダールを実装

---
+++ pages/components/calendar.jsx
@@ -0,0 +1,29 @@
+import React, { useCallback, useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import "@fullcalendar/common/main.css";
+import "@fullcalendar/daygrid/main.css";
+import Modal from "../../components/Modal";
+
+const IndexPage = () => {
+  const [isDisplay, setIsDisplay] = useState(false);
+
+  const log = () => {
+    setIsDisplay(true);
+  };
+
+  return (
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialEvents={[{ title: "initial event", start: new Date() }]}
+        height="100vh"
+        dateClick={() => log()}
+      />
+      <Modal isDisplay={isDisplay} setIsDisplay={setIsDisplay} />
+    </>
+  );
+};
+
+export default IndexPage;
