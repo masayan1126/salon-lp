1d93f79506b44d4be5df7d475f9ebb6c94b29c56
masayan1126 <masa199311266@gmail.com>
2020-12-19 11:38:27 +0900

    モーダルのフェードインアニメーションを実装

--- components/Modal.js
+++ components/Modal.js
@@ -4,10 +4,10 @@ const Modal = (props) => {
 
   return (
     <section className={modalDisplay}>
-      <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="fixed z-10 inset-0 overflow-y-auto uc-fadein">
         <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
           </div>
 
           <div
