bdd4616558902826d7b9a09acb9ffd2121e20a30
masayan1126 <masa199311266@gmail.com>
2020-12-20 21:21:02 +0900

    テス

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyArKhjA5XTSBPoIFZ2f9_pmBFlk01O-G9Q",
+  authDomain: "fitmee-b5838.firebaseapp.com",
+  projectId: "fitmee-b5838",
+  storageBucket: "fitmee-b5838.appspot.com",
+  messagingSenderId: "876494352622",
+  appId: "1:876494352622:web:ee39ac2d64b8944a7883ff",
+  measurementId: "G-ME4YLY7F3K",
+};
