f437d6e5a113f086314ba59003b51ea0c41f341b
masayan1126 <masa199311266@gmail.com>
2020-12-20 21:41:01 +0900

    テスト

--- pages/top.js
+++ pages/top.js
@@ -13,18 +13,15 @@ const Top = () => {
         </div>
         <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Before they sold out
+            専門性を活かしたWEBデザインと
             <br class="hidden lg:inline-block" />
-            readymade gluten
+            コーディングで高品質なプロダクトをお届けします。
           </h1>
           <p class="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+            私たちは、パートナー制度を採用しています。これにより、デザイン・コーディング双方の領域において、より高品質かつ専門性の高いプロダクトを作成する事が可能です。
           </p>
           <div class="flex justify-center">
-            <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button class="inline-flex text-white uc-bg-orange border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               問い合わせする
             </button>
           </div>
